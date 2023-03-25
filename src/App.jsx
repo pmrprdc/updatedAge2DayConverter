962d423b4a4689f147489c6b195226fe4d7d81d7
Pamir Perdeci <pamirperdeci@gmail.com>
2023-03-25 16:28:31 -0400

    app works

--- src/App.jsx
+++ src/App.jsx
@@ -4,30 +4,38 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [age, setAge] = useState("")
+  const [display, setDisplay] = useState("")
+  
 
+  const inputChangeHandler = (e) => {
+    e.preventDefault();
+    setAge(e.target.value)
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    const age1 = parseInt(age) * 365;
+    setDisplay(age1)
+    setAge("")
+    
+  }
+
+  const restartHandler = (e) => {
+    setAge("")
+    setDisplay("")
+  }
+ 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{display}</h1>
+      <h1>How old are you?</h1>
+      <form onSubmit={submitHandler}>
+        <input type="submit" value="submit"/>
+        <input value={age} onChange={inputChangeHandler} type="text"/>
+      </form>
+      <button onClick={restartHandler} style={{
+        color : "red", backgroundColor:"blue"}}>Restart</button>
     </div>
   )
 }
